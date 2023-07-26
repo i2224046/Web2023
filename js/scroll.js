a6b9b792e98f4184f51034ebd56ae2c2209f6642
i2224046 <130639812+i2224046@users.noreply.github.com>
2023-07-26 14:40:24 +0900

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -29,3 +29,24 @@ $('#page-top a').click(function () {
     }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
     return false;//リンク自体の無効化
 });
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200){//上から200pxスクロールしたら
+		$('#page-next').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-next').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-next').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-next').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-next').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+// #page-topをクリックした際の設定
+$('#page-next a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
